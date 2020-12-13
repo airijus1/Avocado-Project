497158d0d2d23b38b4b5f973c971a6c7b61539a9
airijus1 <46110821+airijus1@users.noreply.github.com>
2020-12-13 22:04:59 +0000

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: C:\xampp\htdocs\login.php");	
+    header("location: C:login.php");	
     exit;
 }
 

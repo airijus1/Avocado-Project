03256fefb59cefe99bc5440a1675e42ea38f1d8e
airijus1 <46110821+airijus1@users.noreply.github.com>
2020-12-14 13:21:02 +0000

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,10 +3,10 @@
 session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+/*if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: C:login.php");	
     exit;
-}
+}*/
 
     $conn = new mysqli('localhost', 'root', '', 'demo');
 
